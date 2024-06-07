407f41cda88110e295048fdc7102e94d346dae3d
Tyler Harden <tyler.harden@energyq.com.au>
2024-06-07 16:48:44 +1000

    Dynamic footer social links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,47 +10,47 @@ const Footer = ({ changeTheme, sticky }) => {
         <div className="row justify-content-center">
           <div className="col-12 d-flex flex-wrap justify-content-center">
             <ul className="social-links list-inline text-center">
-              <li className="list-inline-item col-2 col-sm-1">
+              <li className="list-inline-item col-2 col-sm-1 spotify">
                 <a href="https://open.spotify.com/artist/5uXWOfu1kA8mQ9bUp5GgxT" target="_blank" rel="noopener noreferrer">
                   <i className="fab fa-spotify"></i>
                 </a>
               </li>
-              <li className="list-inline-item col-2 col-sm-1">
+              <li className="list-inline-item col-2 col-sm-1 instagram">
                 <a href="https://instagram.com/daste.music" target="_blank" rel="noopener noreferrer">
                   <i className="fab fa-instagram"></i>
                 </a>
               </li>
-              <li className="list-inline-item col-2 col-sm-1">
+              <li className="list-inline-item col-2 col-sm-1 tiktok">
                 <a href="https://tiktok.com/@daste.music" target="_blank" rel="noopener noreferrer">
                   <i className="fab fa-tiktok"></i>
                 </a>
               </li>
-              <li className="list-inline-item col-2 col-sm-1">
+              <li className="list-inline-item col-2 col-sm-1 facebook">
                 <a href="https://www.facebook.com/daste.music" target="_blank" rel="noopener noreferrer">
                   <i className="fab fa-facebook-f"></i>
                 </a>
               </li>
-              <li className="list-inline-item col-2 col-sm-1">
+              <li className="list-inline-item col-2 col-sm-1 twitter">
                 <a href="https://x.com/dasteband" target="_blank" rel="noopener noreferrer">
                   <i className="fab fa-twitter"></i>
                 </a>
               </li>
-              <li className="list-inline-item col-2 col-sm-1">
+              <li className="list-inline-item col-2 col-sm-1 youtube">
                 <a href="https://www.youtube.com/channel/UCOvwkCtPWz9rDC4rcNf303w" target="_blank" rel="noopener noreferrer">
                   <i className="fab fa-youtube"></i>
                 </a>
               </li>
-              <li className="list-inline-item col-2 col-sm-1">
+              <li className="list-inline-item col-2 col-sm-1 soundcloud">
                 <a href="https://soundcloud.com/daste-music" target="_blank" rel="noopener noreferrer">
                   <i className="fab fa-soundcloud"></i>
                 </a>
               </li>
-              <li className="list-inline-item col-2 col-sm-1">
+              <li className="list-inline-item col-2 col-sm-1 bandcamp">
                 <a href="https://daste.bandcamp.com/" target="_blank" rel="noopener noreferrer">
                   <i className="fab fa-bandcamp"></i>
                 </a>
               </li>
-              <li className="list-inline-item col-2 col-sm-1">
+              <li className="list-inline-item col-2 col-sm-1 email">
                 <a href="mailto:info@mammalsounds.com">
                   <i className="fas fa-envelope"></i>
                 </a>

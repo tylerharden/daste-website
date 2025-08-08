beb9ad51ed2fea7b82daa116b6b9b9e5d68d5235
Tyler Harden <tyler-harden@outlook.com>
2025-08-08 22:09:00 +1000

    Updated music page with a latestUpdateCard (not being used yet)

--- src/components/Music/Music.js
+++ src/components/Music/Music.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import './Music.css';
 import SpotifyEmbed from './SpotifyEmbed';
+import LatestUpdateCard from '../LatestUpdateCard';
 
 const Music = ({ theme, changeTheme }) => {
   return (
@@ -11,6 +12,16 @@ const Music = ({ theme, changeTheme }) => {
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.8, ease: 'easeOut' }}
     >
+      {/* <LatestUpdateCard
+        artworkSrc="/images/dasteWORLD_Album_Artwork.jpeg"
+        links={{
+          spotify: "https://open.spotify.com/album/...", 
+          apple: "https://music.apple.com/album/...",
+          youtube: "https://music.youtube.com/playlist?list=...",
+          bandcamp: "https://daste.bandcamp.com/album/dasteworld"
+        }}
+      /> */}
+
       <motion.div
         className="video-wrapper"
         initial={{ opacity: 0, y: 30 }}
@@ -23,9 +34,10 @@ const Music = ({ theme, changeTheme }) => {
           title="YouTube video player" 
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
           referrerpolicy="strict-origin-when-cross-origin" 
-          allowfullscreen>
-        </iframe>
+          allowFullScreen
+        />
       </motion.div>
+
       <motion.div
         className="spotify-embed"
         initial={{ opacity: 0, y: 30 }}
@@ -33,79 +45,10 @@ const Music = ({ theme, changeTheme }) => {
         viewport={{ once: true, amount: 0.2 }}
         transition={{ duration: 0.8, delay: 0.4, ease: 'easeOut' }}
       >
-        <SpotifyEmbed artistId={'5uXWOfu1kA8mQ9bUp5GgxT'}/>
+        <SpotifyEmbed artistId={'5uXWOfu1kA8mQ9bUp5GgxT'} />
       </motion.div>
     </motion.div>
   );
 }
 
 export default Music;
-
-
-// <main className="music">
-//         <section>
-//           {/* <h2>Around (Single)</h2> */}
-//           <iframe 
-//             title="Around Single by daste." 
-//             src="https://open.spotify.com/embed/album/5sfmsDl4lB1lZdX2gxIK3q?si=LiNlxqvrQMSXzbRtBbTRqg" 
-//             width="100%" 
-//             height="380" 
-//             frameBorder="0" 
-//             allowTransparency="true" 
-//             allow="encrypted-media"
-//           ></iframe>
-//           {/* <img src="assets/around.jpg" alt="Around Single" className="release-photo" /> */}
-//         </section>
-//         <section>
-//           {/* <h2>Butterfly (Single)</h2> */}
-//           <iframe 
-//             title="Butterfly Single by daste." 
-//             src="https://open.spotify.com/embed/album/5Q38YvTGu832sX1ZDKK2sO?si=_1WIx-6cSvu540d5yMjwwQ" 
-//             width="100%" 
-//             height="380" 
-//             frameBorder="0" 
-//             allowTransparency="true" 
-//             allow="encrypted-media"
-//           ></iframe>
-//           {/* <img src="assets/butterfly.jpg" alt="Butterfly Single" className="release-photo" /> */}
-//         </section>
-//         <section>
-//           {/* <h2>Dusk & Dawn (Album)</h2> */}
-//           <iframe 
-//             title="Dusk & Dawn Album by daste." 
-//             src="https://open.spotify.com/embed/album/77bVy7O8N5R9vSDhcXWPFb?si=2qzmvx_KQhawv_f1GfFThw" 
-//             width="100%" 
-//             height="380" 
-//             frameBorder="0" 
-//             allowTransparency="true" 
-//             allow="encrypted-media"
-//           ></iframe>
-//           {/* <img src="dusk_dawn.jpg" alt="Dusk & Dawn Album" className="release-photo" /> */}
-//         </section>
-//         <section>
-//           {/* <h2>Same As It Ever Was (EP)</h2> */}
-//           <iframe 
-//             title="Same As It Ever Was EP by daste." 
-//             src="https://open.spotify.com/embed/album/5UwvFKxjw0hwptJ5HyOWNb?si=u-xgaqHwRKyIBpSrCHCcJQ" 
-//             width="100%" 
-//             height="380" 
-//             frameBorder="0" 
-//             allowTransparency="true" 
-//             allow="encrypted-media"
-//           ></iframe>
-//           {/* <img src="assets/same_as_it_ever_was.jpg" alt="Same As It Ever Was EP" className="release-photo" /> */}
-//         </section>
-//         <section>
-//           {/* <h2>Palette (EP)</h2> */}
-//           <iframe 
-//             title="Palette EP by daste." 
-//             src="https://open.spotify.com/embed/album/5vAb08N0DcBLIX2QopJbOa?si=IiM576EUS9O_SpusG9BUwA" 
-//             width="100%" 
-//             height="380" 
-//             frameBorder="0" 
-//             allowTransparency="true" 
-//             allow="encrypted-media"
-//           ></iframe>
-//           {/* <img src="assets/palette.jpg" alt="Palette EP" className="release-photo" /> */}
-//         </section>
-//       </main>
